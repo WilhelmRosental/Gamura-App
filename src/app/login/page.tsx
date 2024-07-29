932b7c5108d5369a540ceae569ffdedfaf4c77e0
Nathan Hallouin <wilhelm.rosental@ik.me>
2024-07-29 02:20:49 +0200

    login & signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,66 +1,36 @@
 "use client";
 
-import { useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
-import { Button, Card } from "@radix-ui/themes";
-import {
-  DiscordLogoIcon,
-  EnvelopeClosedIcon,
-  TextField,
-} from "@radix-ui/react-icons";
+import { login, signup } from "./actions";
+import { Button, Card, TextField } from "@radix-ui/themes";
+import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-
-  const handleLoginWithEmail = async () => {
-    const { error } = await supabase.auth.signInWithOtp({ email });
-    if (error) {
-      alert(error.message);
-    } else {
-      alert("Check your email for the login link!");
-    }
-  };
-
-  const handleLoginWithDiscord = async () => {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: "discord",
-    });
-    if (error) {
-      alert(error.message);
-    }
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Card className="p-6 rounded-lg shadow-lg max-w-md w-full">
         <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
-        <div className="flex flex-col gap-4">
-          {/* <input
-            type="email"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-            className="p-2 rounded bg-gray-700 border border-gray-600"
-          /> */}
+        <form className="flex flex-col gap-4">
           <TextField.Root
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="email"
             size="1"
             placeholder="Enter your email"
           />
-          <Button onClick={handleLoginWithEmail}>
+          <TextField.Root
+            type="password"
+            name="password"
+            size="1"
+            placeholder="Enter your password"
+          />
+          <Button formAction={login}>
             <EnvelopeClosedIcon />
-            Login with Email
+            Login
           </Button>
-          <Button
-            onClick={handleLoginWithDiscord}
-            className="p-2 rounded flex items-center justify-center gap-2"
-          >
-            <DiscordLogoIcon />
-            Login with Discord
+          <Button formAction={signup}>
+            <EnvelopeClosedIcon />
+            Sign Up
           </Button>
-        </div>
+        </form>
       </Card>
     </div>
   );
